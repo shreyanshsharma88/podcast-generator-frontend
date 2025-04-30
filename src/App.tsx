15ef26769284dff86bade9ea3f084398314b597b
shreyanshSharma88 <shreyanshsharma480@gmail.com>
2025-04-30 11:43:04 +0530

    audio integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,145 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import { BASE_URL, ELEVEN_API_KEY } from "./constants";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <>
+      <PodcastGenerator />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
+
+const PodcastGenerator = () => {
+  console.log("BASE_URL", BASE_URL);
+  console.log("ELEVEN", ELEVEN_API_KEY);
+  const [query, setQuery] = useState<string>("");
+  const [response, setResponse] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const handleSubmit = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post(`${BASE_URL}/generate-podcast`, {
+        query,
+      });
+
+      if (!res.data.content) {
+        toast.error("GenAI model overloaded");
+        setLoading(false);
+        return;
+      }
+
+      setResponse(res.data.content);
+      setLoading(false);
+      console.log({ res, response });
+    } catch (error) {
+      toast.error("GenAI model overloaded");
+      setLoading(false);
+      console.error("Error:", error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <input
+        onChange={(e) => setQuery(e.target.value)}
+        style={{
+          width: "100%",
+          height: "30px",
+          fontSize: "20px",
+          padding: "10px",
+          marginBottom: "20px",
+        }}
+        placeholder="Enter your query here..."
+      />
+      {loading ? (
+        <div>Creating Podcast...</div>
+      ) : (
+        !response && <button onClick={handleSubmit}>Generate</button>
+      )}
+      <div
+        style={{
+          maxHeight: "600px",
+          overflow: "auto",
+        }}
+      >
+        {response}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {response && <SpeakPodcast query={response} />}
     </>
-  )
-}
+  );
+};
 
-export default App
+const SpeakPodcast = ({ query }: { query: string }) => {
+  const [loading, setLoading] = useState(false);
+  const getPlayableAudio = async () => {
+    const response = await axios.post(
+      `https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM`,
+      {
+        text: query,
+        model_id: "eleven_monolingual_v1", // or 'eleven_multilingual_v1'
+        voice_settings: {
+          stability: 0.5,
+          similarity_boost: 0.75,
+        },
+      },
+      {
+        headers: {
+          "xi-api-key": ELEVEN_API_KEY,
+          "Content-Type": "application/json",
+          Accept: "audio/mpeg",
+        },
+        responseType: "blob",
+      }
+    );
+
+    return response.data;
+  };
+
+  const handlePlay = async () => {
+    try {
+      setLoading(true);
+      const audioBlob = await getPlayableAudio();
+      const audioUrl = URL.createObjectURL(audioBlob);
+      const audio = new Audio(audioUrl);
+      audio.play();
+      setLoading(false);
+    } catch (error) {
+      console.error("TTS failed:", error);
+    }
+  };
+  return (
+    <div style={{
+      marginTop: "20px",
+    }}>
+      {loading ? (
+        <div>Please wait, generating playable audio...</div>
+      ) : (
+        <img
+          onClick={handlePlay}
+          src="https://cdn-icons-png.flaticon.com/512/59/59284.png"
+          style={{
+            height: "50px",
+            width: "50px",
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL
+export const ELEVEN_API_KEY = import.meta.env.VITE_ELEVEN_API_KEY
